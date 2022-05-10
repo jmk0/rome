6c5a72052d32e4f4d19ce73e21378f838e60d4cc
HelgeKeck <helgekeck@hotmail.com>
2022-05-10 03:02:29 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# ratos-multi-extruder
-Multi extruder to direct extruder setup
+# ROME
+RatOS Multi Extruder
+
+A multi extruder to direct extruder solution for RatOS
+
+# Installation
+
+## On your Raspberry
+```
+cd ~/
+git clone https://github.com/HelgeKeck/rome.git
+bash ~/rome/install.sh
+```
+
+## Configure Moonraker update manager
+```ini
+# moonraker.conf
+
+[update_manager rome]
+type: git_repo
+primary_branch: main
+path: ~/rome
+origin: https://github.com/HelgeKeck/rome.git
+```
+
+## Activate ROME in your Klipper printer.cfg 
+```ini
+# printer.cfg
+
+# ROME
+[include rome/config.cfg]
+
+```
---
+++ install.sh
@@ -0,0 +1,110 @@
+# Prevent running as root.
+if [ ${UID} == 0 ]; then
+    echo -e "DO NOT RUN THIS SCRIPT AS 'root' !"
+    echo -e "If 'root' privileges needed, you will prompted for sudo password."
+    exit 1
+fi
+
+# Force script to exit if an error occurs
+set -e
+
+# Find SRCDIR from the pathname of this script
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
+
+# Default Parameters
+KLIPPER_CONFIG_DIR="${HOME}/klipper_config"
+KLIPPY_EXTRAS="${HOME}/klipper/klippy/extras"
+ROME_DIR="${HOME}/klipper_config/rome"
+ROME_MACROS_DIR="${HOME}/klipper_config/rome/macros"
+ROME_HARDWARE_DIR="${HOME}/klipper_config/rome/hardware"
+
+function stop_klipper {
+    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
+        echo "Klipper service found! Stopping during Install."
+        sudo systemctl stop klipper
+    else
+        echo "Klipper service not found, please install Klipper first"
+        exit 1
+    fi
+}
+
+function start_klipper {
+    echo "Restarting Klipper service!"
+    sudo systemctl restart klipper
+}
+
+function create_rome_dir {
+    if [ -d "${ROME_DIR}" ]; then
+        rm -rf "${ROME_DIR}" 
+    fi
+    if [ -d "${KLIPPER_CONFIG_DIR}" ]; then
+        echo "Creating rome folder..."
+        mkdir "${ROME_DIR}"
+        mkdir "${ROME_MACROS_DIR}"
+        mkdir "${ROME_MACROS_DIR}/tools"
+        mkdir "${ROME_MACROS_DIR}/print"
+        mkdir "${ROME_MACROS_DIR}/load"
+        mkdir "${ROME_HARDWARE_DIR}"
+    else
+        echo -e "ERROR: ${KLIPPER_CONFIG_DIR} not found."
+        exit 1
+    fi
+}
+
+function link_rome_macros {
+    if [ -d "${KLIPPER_CONFIG_DIR}" ]; then
+        if [ -d "${ROME_DIR}" ]; then
+            echo "Linking macro files..."
+            ln -sf "${SRCDIR}/klipper_macro/config.cfg" "${ROME_DIR}/config.cfg"
+            ln -sf "${SRCDIR}/klipper_macro/exchange.cfg" "${ROME_DIR}/exchange.cfg"
+            ln -sf "${SRCDIR}/klipper_macro/hardware.cfg" "${ROME_DIR}/hardware.cfg"
+            ln -sf "${SRCDIR}/klipper_macro/macros.cfg" "${ROME_DIR}/macros.cfg"
+            ln -sf "${SRCDIR}/klipper_macro/ERCF_FORM_TIP_STANDALONE.cfg" "${ROME_DIR}/ERCF_FORM_TIP_STANDALONE.cfg"
+        else
+            echo -e "ERROR: ${ROME_DIR} not found."
+            exit 1
+        fi
+    else
+        echo -e "ERROR: ${KLIPPER_CONFIG_DIR} not found."
+        exit 1
+    fi
+}
+
+function link_rome_extras {
+    if [ -d "${KLIPPY_EXTRAS}" ]; then
+        echo "Linking extra file..."
+        ln -sf "${SRCDIR}/klipper_extra/rome.py" "${KLIPPY_EXTRAS}/rome.py"
+    else
+        echo -e "ERROR: ${KLIPPY_EXTRAS} not found."
+        exit 1
+    fi
+}
+
+### MAIN
+
+# Parse command line arguments
+while getopts "c:h" arg; do
+    if [ -n "${arg}" ]; then
+        case $arg in
+            c)
+                KLIPPER_CONFIG_DIR=$OPTARG
+                break
+            ;;
+            [?]|h)
+                echo -e "\nUsage: ${0} -c /path/to/klipper_config"
+                exit 1
+            ;;
+        esac
+    fi
+    break
+done
+
+# Run steps
+stop_klipper
+create_rome_dir
+link_rome_macros
+link_rome_extras
+start_klipper
+
+# If something checks status of install
+exit 0
